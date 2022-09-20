91133382d27a7ce5964a9f3a31e354df8cc97819
GencVahti <vahtigenc@hotmail.com>
2022-09-20 22:54:36 +0300

    userdata.sh added

---
+++ userdata.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+yum update -y
+yum install python3 -y
+pip3 install flask
+cd /home/ec2-user
+wget -P templates https://raw.githubusercontent.com/GencVahti/Project-02-roman-numerals-converter/main/templates/index.html
+wget -P templates https://raw.githubusercontent.com/GencVahti/Project-02-roman-numerals-converter/main/templates/result.html
+wget https://raw.githubusercontent.com/GencVahti/Project-02-roman-numerals-converter/main/app.py
+python3 app.py
